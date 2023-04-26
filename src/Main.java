5f67e2f68bbe5f08feeeb442d5ba4d45657750b4
nandpatel <nandpat0107@gmail.com>
2023-04-25 19:42:34 -0500

    added layered pane

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,32 @@ public class Main {
         JLayeredPane tab = new JLayeredPane();
         tab.setSize(1440,900);
 
+        JLayeredPane whe = new JLayeredPane();
+        whe.setSize(200,200);
+
 
 
         ImageIcon icon = new ImageIcon("tabless.png");
         icon.setImage(icon.getImage().getScaledInstance(1440,900,Image.SCALE_DEFAULT));
         JLabel table = new JLabel(icon);
-        tab.add(table,Integer.valueOf(0));
+        tab.add(table,Integer.valueOf(1));
         table.setBounds(0,0,1440,900);
+
+        ImageIcon ico = new ImageIcon("wheel.png");
+        ico.setImage(ico.getImage().getScaledInstance(200 ,200,Image.SCALE_DEFAULT));
+        JLabel wheel = new JLabel(ico);
+        whe.add(wheel,Integer.valueOf(0));
+        wheel.setBounds(0,0,200,200);
 
 
 
 
         JFrame myFrame = new JFrame();
 
+
         myFrame.add(tab);
+        myFrame.add(whe);
+
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myFrame.setSize(2500, 1600);
         myFrame.setLayout(null);
